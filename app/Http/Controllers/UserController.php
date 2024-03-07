23841db4cc9390b7d9f61a995911fd0a34c0c1b3
Naufal Haidar <naufalsetyawan015@gmail.com>
2024-03-07 22:09:43 +0700

    Jobsheet 4 - Praktikum 2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -134,7 +134,11 @@ class UserController extends Controller
         // $user->wasChanged('nama');
         // dd($user->wasChanged(['nama', 'username']));
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::with('level')->get();
+        // dd($user);
         return view('user', ['data' => $user]);
     }
 
