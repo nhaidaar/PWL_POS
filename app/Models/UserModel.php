f11d9769768d8da4b1a3481b1e6c67dbcecbaff7
Naufal Haidar <naufalsetyawan015@gmail.com>
2024-04-23 23:27:49 +0700

    feat: Jobsheet 10 - Restful API

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,14 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends User
+class UserModel extends User implements JWTSubject
 {
-    use HasFactory;
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+use App\Http\Controllers\Api\LevelController;
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+use App\Http\Controllers\Api\KategoriController;
+Route::get('categories', [KategoriController::class, 'index']);
+Route::post('categories', [KategoriController::class, 'store']);
+Route::get('categories/{kategori}', [KategoriController::class, 'show']);
+Route::put('categories/{kategori}', [KategoriController::class, 'update']);
+Route::delete('categories/{kategori}', [KategoriController::class, 'destroy']);
+
+use App\Http\Controllers\Api\BarangController;
+Route::get('stuffs', [BarangController::class, 'index']);
+Route::post('stuffs', [BarangController::class, 'store']);
+Route::get('stuffs/{barang}', [BarangController::class, 'show']);
+Route::put('stuffs/{barang}', [BarangController::class, 'update']);
+Route::delete('stuffs/{barang}', [BarangController::class, 'destroy']);
